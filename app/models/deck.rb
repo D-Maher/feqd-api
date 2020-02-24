08f2fa8eabbed44ccbfc50c429b4048c5c9eb251
D-Maher <dmaher724@gmail.com>
2020-02-23 18:45:47 -0600

    Annotate models, model specs, and fixtures

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: decks
+#
+#  id          :bigint           not null, primary key
+#  description :string
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Deck < ApplicationRecord
 
   has_many :card_memberships
